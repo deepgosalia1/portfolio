f4fd36429e126c63021c49d64706cc4ce84a3c3a
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2020-07-31 05:17:57 +0530

    Updates

---
+++ precache-manifest.e1558c048bdf0623af3a1b2658beaff1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c36d5cf6c5461ddab2d1a84ae7daa4a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "426cbe776faa3af693fb",
+    "url": "/portfolio/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "da44da6d7109044d1835",
+    "url": "/portfolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "58392fb882904c8b76ce",
+    "url": "/portfolio/static/css/main.b9ba990e.chunk.css"
+  },
+  {
+    "revision": "1ca27916bada012ef02f",
+    "url": "/portfolio/static/js/2.801a461f.chunk.js"
+  },
+  {
+    "revision": "426cbe776faa3af693fb",
+    "url": "/portfolio/static/js/3.dadaac71.chunk.js"
+  },
+  {
+    "revision": "da44da6d7109044d1835",
+    "url": "/portfolio/static/js/4.fcebc514.chunk.js"
+  },
+  {
+    "revision": "58392fb882904c8b76ce",
+    "url": "/portfolio/static/js/main.1a7589b5.chunk.js"
+  },
+  {
+    "revision": "0d02f78e4f027b061f12",
+    "url": "/portfolio/static/js/runtime-main.d37e6d79.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d9cf7abea8eb8e64004ed0759897fba3",
+    "url": "/portfolio/static/media/codeaxesLogo.d9cf7abe.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a9c34e8953367a7924f0271ec61fa409",
+    "url": "/portfolio/static/media/myphoto.a9c34e89.jpg"
+  },
+  {
+    "revision": "c58d8ee60dfaac20c342740b87817346",
+    "url": "/portfolio/static/media/nlp.c58d8ee6.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "4c2d394947b764a0fc00668e1f434bdc",
+    "url": "/portfolio/static/media/speridian.4c2d3949.png"
+  }
+]);
